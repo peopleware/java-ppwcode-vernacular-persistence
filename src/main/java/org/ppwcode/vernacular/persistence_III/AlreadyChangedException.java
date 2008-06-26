6b096b4ea1abf7f732ff96892342abeb5f2b4958
Jan Dockx <jan_dockx@peopleware.be>
2008-06-26 10:34:00 +0000

    More setup of good errors and exceptions. (also for exception vernacular, to be copied over later.)
    git-svn-id: file:///repositories/PPWCode/java/vernacular/persistence/trunk@1373 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/main/java/org/ppwcode/vernacular/persistence_III/AlreadyChangedException.java
@@ -0,0 +1,82 @@
+/*<license>
+Copyright 2004 - $Date: 2008-06-26 11:53:54 +0200 (Thu, 26 Jun 2008) $ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+</license>*/
+
+package org.ppwcode.vernacular.persistence_III;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.toryt.annotations_I.Basic;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.Invars;
+import org.toryt.annotations_I.MethodContract;
+
+
+/**
+ * <p>This exception signals a versioning conflict: we tried to change data in persistent
+ *   storage for {@link #getPersistentBean()}, but the data in persistent storage has
+ *   changed already, since the last time we looked. We will not overwrite this more
+ *   recent data without user interaction.</p>
+ *
+ * @author    Jan Dockx
+ * @author    PeopleWare n.v.
+ */
+@Copyright("2004 - $Date: 2008-06-26 11:53:54 +0200 (Thu, 26 Jun 2008) $, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision: 1370 $",
+         date     = "$Date: 2008-06-26 11:53:54 +0200 (Thu, 26 Jun 2008) $")
+public class AlreadyChangedException extends PersistenceException {
+
+  /*<construction>*/
+  //------------------------------------------------------------------
+
+  @MethodContract(
+    pre = {
+      @Expression("persistentBean != null")
+    },
+    post = {
+      @Expression("persistentBean == _persistentBeanType"),
+      @Expression("message == _message"),
+      @Expression("cause == _cause")
+    }
+  )
+  public AlreadyChangedException(PersistentBean<?> persistentBean, String message, Throwable cause) {
+    super(message, cause);
+    assert persistentBean != null;
+    $persistentBean = persistentBean;
+  }
+
+  /*</construction;>*/
+
+
+
+  /*<property name="persistentBean">*/
+  //------------------------------------------------------------------
+
+  @Basic(invars = @Expression("persistentBean != null"))
+  public final PersistentBean<?> getPersistentBeanType() {
+    return $persistentBean;
+  }
+
+  @Invars(@Expression("$persistentBean != null"))
+  private final PersistentBean<?> $persistentBean;
+
+  /*</property>*/
+
+}
