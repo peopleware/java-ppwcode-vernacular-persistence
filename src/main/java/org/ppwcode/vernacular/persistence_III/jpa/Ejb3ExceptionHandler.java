a9f8a4163fe6e3dc04860c22a9d0abd9b7e58dcc
ppw.bot <ppw.bot@peopleware.be>
2008-10-09 10:43:42 +0000

    svn_spider [r1824]
    working root:
      https://ppwcode.googlecode.com/svn/java/vernacular/persistence/trunk
    
    fixes:
      src/main/java/org/ppwcode/vernacular/persistence_III/jpa/Ejb3ExceptionHandler.java
        +svn:eol-style:       native
        +svn:keywords:        Date Rev
        +svn:mime-type:       text/x-java-source
    
    
    git-svn-id: file:///repositories/PPWCode/java/vernacular/persistence/trunk@2971 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/persistence_III/jpa/Ejb3ExceptionHandler.java
+++ src/main/java/org/ppwcode/vernacular/persistence_III/jpa/Ejb3ExceptionHandler.java
@@ -1,5 +1,5 @@
 /*<license>
-Copyright 2004 - $Date: 2008-10-06 16:44:16 +0200 (Mon, 06 Oct 2008) $ by PeopleWare n.v..
+Copyright 2004 - $Date$ by PeopleWare n.v..
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
