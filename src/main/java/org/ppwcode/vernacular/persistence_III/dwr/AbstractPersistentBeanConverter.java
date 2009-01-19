0f5cbfe366ade03cd25a90c0ec03403bf32851e2
Tom.Mahieu <Tom.Mahieu@peopleware.be>
2009-01-19 15:13:13 +0000

    Persistent Bean converter: * triages Value Exceptions and assigns them to the property being unmarshalled * catches and collects PropertyExceptions * throws CompoundException after all setters have been executed
    git-svn-id: file:///repositories/PPWCode/java/vernacular/persistence/trunk@4017 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/persistence_III/dwr/AbstractPersistentBeanConverter.java
+++ src/main/java/org/ppwcode/vernacular/persistence_III/dwr/AbstractPersistentBeanConverter.java
@@ -1,5 +1,7 @@
 package org.ppwcode.vernacular.persistence_III.dwr;
 
+import static org.ppwcode.util.exception_III.ExceptionHelpers.huntFor;
+
 import java.util.Iterator;
 import java.util.Map;
 
@@ -14,6 +16,9 @@ import org.directwebremoting.extend.TypeHintContext;
 import org.directwebremoting.util.LocalUtil;
 import org.directwebremoting.util.Logger;
 import org.directwebremoting.util.Messages;
+import org.ppwcode.vernacular.semantics_VI.exception.CompoundPropertyException;
+import org.ppwcode.vernacular.semantics_VI.exception.PropertyException;
+import org.ppwcode.vernacular.value_III.ValueException;
 
 /**
  * @author tmahieu
@@ -34,43 +39,33 @@ public abstract class AbstractPersistentBeanConverter extends BeanConverter {
 		String value = iv.getValue();
 
 		// If the text is null then the whole bean is null
-		if (value.trim().equals(ProtocolConstants.INBOUND_NULL))
-		{
+		if (value.trim().equals(ProtocolConstants.INBOUND_NULL)) {
 			return null;
 		}
 
-		if (!value.startsWith(ProtocolConstants.INBOUND_MAP_START))
-		{
+		if (!value.startsWith(ProtocolConstants.INBOUND_MAP_START)) {
 			throw new MarshallException(paramType, Messages.getString("BeanConverter.FormatError", ProtocolConstants.INBOUND_MAP_START));
 		}
 
-		if (!value.endsWith(ProtocolConstants.INBOUND_MAP_END))
-		{
+		if (!value.endsWith(ProtocolConstants.INBOUND_MAP_END)) {
 			throw new MarshallException(paramType, Messages.getString("BeanConverter.FormatError", ProtocolConstants.INBOUND_MAP_START));
 		}
 
 		value = value.substring(1, value.length() - 1);
 
-		try
-		{
+		try {
 			Object bean;
-			if (instanceType != null)
-			{
+			if (instanceType != null) {
 				bean = instanceType.newInstance();
-			}
-			else
-			{
+			} else {
 				bean = paramType.newInstance();
 			}
 
 			// We should put the new object into the working map in case it
 			// is referenced later nested down in the conversion process.
-			if (instanceType != null)
-			{
+			if (instanceType != null) {
 				inctx.addConverted(iv, instanceType, bean);
-			}
-			else
-			{
+			}	else {
 				inctx.addConverted(iv, paramType, bean);
 			}
 
@@ -78,15 +73,14 @@ public abstract class AbstractPersistentBeanConverter extends BeanConverter {
 
 			// Loop through the properties passed in
 			Map tokens = extractInboundTokens(paramType, value);
-			for (Iterator it = tokens.entrySet().iterator(); it.hasNext();)
-			{
+			CompoundPropertyException cpe = new CompoundPropertyException(bean, null, null, null);
+			for (Iterator it = tokens.entrySet().iterator(); it.hasNext();) {
 				Map.Entry entry = (Map.Entry) it.next();
 				String key = (String) entry.getKey();
 				String val = (String) entry.getValue();
 
 				Property property = (Property) properties.get(key);
-				if (property == null)
-				{
+				if (property == null) {
 					log.warn("Missing java bean property to match javascript property: " + key + ". For causes see debug level logs:");
 
 					log.debug("- The javascript may be refer to a property that does not exist");
@@ -94,11 +88,9 @@ public abstract class AbstractPersistentBeanConverter extends BeanConverter {
 					log.debug("- The property may be excluded using include or exclude rules.");
 
 					StringBuffer all = new StringBuffer();
-					for (Iterator pit = properties.keySet().iterator(); pit.hasNext();)
-					{
+					for (Iterator pit = properties.keySet().iterator(); pit.hasNext();) {
 						all.append(pit.next());
-						if (pit.hasNext())
-						{
+						if (pit.hasNext()) {
 							all.append(',');
 						}
 					}
@@ -119,22 +111,62 @@ public abstract class AbstractPersistentBeanConverter extends BeanConverter {
 				InboundVariable nested = new InboundVariable(iv.getLookup(), null, splitType, splitValue);
 				TypeHintContext incc = createTypeHintContext(inctx, property);
 
-				Object output = converterManager.convertInbound(propType, nested, inctx, incc);
-				property.setValue(bean, output);
+				try {
+					Object output = converterManager.convertInbound(propType, nested, inctx, incc);
+					property.setValue(bean, output);
+				} catch (MarshallException mex) {
+					//collect all exceptions
+					triageExceptions(bean, property, mex, cpe);
+				} 
 			}
-
+			//throw the compound exception if it's not empty
+			if (cpe.getSize() > 0) {
+				cpe.close();
+				throw cpe;
+			}
 			return bean;
-		}
-		catch (MarshallException ex)
-		{
+		} catch (MarshallException ex) {
 			throw ex;
-		}
-		catch (Exception ex)
-		{
+		} catch (Exception ex) {
 			throw new MarshallException(paramType, ex);
 		}
 	}
 
+	private void triageExceptions(Object bean,
+			                          Property property,
+			                          MarshallException subject,
+			                          CompoundPropertyException compound) throws MarshallException {
+		Exception e = null;
+		// First check for ValueExceptions
+		e = huntFor(subject, ValueException.class);
+		if (e != null) {
+			compound.addElementException(
+					new PropertyException(
+							bean,
+							property.getName(),
+							e.getLocalizedMessage(), 
+							e)); 
+		} else {
+			// No Value Exceptions found, check for CompoundPropertyExceptions
+			e = huntFor(subject, CompoundPropertyException.class);
+			if (e != null) {
+				for ( Iterator<PropertyException> it = ((CompoundPropertyException)e).getElementExceptions().iterator();
+				      it.hasNext();) {
+					compound.addElementException(it.next());
+				}
+			} else { 
+				// No CompoundPropertyExceptions, what about a PropertyException?
+				e = huntFor(subject, PropertyException.class);
+				if (e != null) {
+					compound.addElementException((PropertyException)e);
+				} else {
+					// Hmm what to do what to do... throw a programming error?
+					throw subject;
+				}
+			}
+		}
+	}
+	
 	/**
 	 * The log stream
 	 */
