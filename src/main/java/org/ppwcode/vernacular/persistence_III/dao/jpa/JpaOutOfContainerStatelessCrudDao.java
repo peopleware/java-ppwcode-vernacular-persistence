429beb7bc04e94f2875dcce6aa3d9f32138e7e29
Jan Dockx <jan_dockx@peopleware.be>
2009-06-22 20:22:33 +0000

    fixed import warning
    git-svn-id: file:///repositories/PPWCode/java/vernacular/persistence/trunk@4383 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/vernacular/persistence_III/dao/jpa/JpaOutOfContainerStatelessCrudDao.java
+++ src/main/java/org/ppwcode/vernacular/persistence_III/dao/jpa/JpaOutOfContainerStatelessCrudDao.java
@@ -3,7 +3,6 @@ package org.ppwcode.vernacular.persistence_III.dao.jpa;
 import javax.persistence.EntityManager;
 
 import org.ppwcode.util.exception_III.ProgrammingErrorHelpers;
-import org.springframework.test.annotation.ExpectedException;
 import org.toryt.annotations_I.Basic;
 import org.toryt.annotations_I.Expression;
 import org.toryt.annotations_I.MethodContract;
@@ -13,52 +12,52 @@ import org.toryt.annotations_I.MethodContract;
  * will be programmatically created, instead of being injected by a container.
  * The JpaStatelessCrudDao must be configured with an entity manager using
  * a setter.
- * 
+ *
  * Note that transaction control is still left out.  It is at this
  * moment not decided whether the CRUD operation should run in its own transaction
  * or will be part of another transaction.
- * 
+ *
  * Note that the rollbackOnly implementation can be provided since we will
  * use EntityTransactions in this application
- * 
+ *
  * @author tmahieu
  *
  */
 public class JpaOutOfContainerStatelessCrudDao extends JpaStatelessCrudDao {
-	
-	
-	/*<property name="entity manager
-	  --------------------------------------------------------------------- */
 
-	@Override
-	@Basic
-	public EntityManager getEntityManager() {
-		return $entityManager;
-	}
 
-	@MethodContract(
-			post=@Expression("entityManager == _entitymanager")
-	)
-	public void setEntityManager(EntityManager entitymanager) {
-		$entityManager = entitymanager;
-	}
+  /*<property name="entity manager
+    --------------------------------------------------------------------- */
 
-	private EntityManager $entityManager = null;
-	/*</property>*/
+  @Override
+  @Basic
+  public EntityManager getEntityManager() {
+    return $entityManager;
+  }
 
-	@Override
-	protected boolean getRollbackOnlyImpl() {
-		// TODO Auto-generated method stub
-		return getEntityManager().getTransaction().getRollbackOnly();
-	}
+  @MethodContract(
+      post=@Expression("entityManager == _entitymanager")
+  )
+  public void setEntityManager(EntityManager entitymanager) {
+    $entityManager = entitymanager;
+  }
 
-	@Override
-	protected void rollbackOnlyPrecondition() throws AssertionError {
-		ProgrammingErrorHelpers.dependency(getEntityManager(), "entityManager");
-	}
+  private EntityManager $entityManager = null;
+  /*</property>*/
 
-	@Override
-	protected void setRollbackOnlyImpl() throws IllegalStateException {
-		getEntityManager().getTransaction().setRollbackOnly();
-	}
+  @Override
+  protected boolean getRollbackOnlyImpl() {
+    // TODO Auto-generated method stub
+    return getEntityManager().getTransaction().getRollbackOnly();
+  }
+
+  @Override
+  protected void rollbackOnlyPrecondition() throws AssertionError {
+    ProgrammingErrorHelpers.dependency(getEntityManager(), "entityManager");
+  }
+
+  @Override
+  protected void setRollbackOnlyImpl() throws IllegalStateException {
+    getEntityManager().getTransaction().setRollbackOnly();
+  }
 }
